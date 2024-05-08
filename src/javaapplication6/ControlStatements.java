d67a7c85450b0e811f92279d08a8641033a3b9cc
reddysatwika <168794907+reddysatwika@users.noreply.github.com>
2024-05-07 21:41:24 -0500

    first commit

---
+++ src/javaapplication6/ControlStatements.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author satwi
+ */
+public class ControlStatements {
+    public static void main(String[] args) {
+//        int x=20;
+//        int y=10;
+//        if(x>y){
+//            System.out.println("x is greater than y");
+//        }
+//        else{
+//            System.out.println("x is less than y");
+//        }
+//        
+        ///Output good evening
+//        int time =22;
+//        if(time<10){
+//            System.out.println("Good Morning");
+//        }
+//        else if(time<18){
+//            System.out.println("Good Afternoon");
+//           
+//        }
+//        else{
+//            System.out.println("Good Evening");
+        //}
+        
+        //Switch statements
+//        int day =7;
+//        switch(day){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//                case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//        case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("This is not a day");
+//
+//        }
+
+    //while loop
+//    int i=0;
+//    while(i<5){
+//        System.out.println(i);
+//        i++;
+//    }
+//    
+    //do-while loop
+    int r=0;
+    do{
+        System.out.println(r);
+        r++;
+    }
+    while(r<5);
+        
+    }
+            
+}
